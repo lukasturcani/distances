21ea7dad0f89bc724ef7bc684b7fda53f7e50e8e
Lukas Turcani <lukasturcani@mailbox.org>
2022-10-20 10:47:50 +0100

    Initial implementation

---
+++ benches/get_distances.rs
@@ -0,0 +1,29 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use distances::Vector3;
+
+fn benchmark_get_distances(c: &mut Criterion) {
+    let mut group = c.benchmark_group("get_distances");
+    let sizes = vec![5, 10, 100, 1_000];
+    for size in sizes {
+        let points = get_points(size);
+        group.throughput(Throughput::Elements(size));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &points, |b, points| {
+            b.iter(|| {
+                let _distances: Vec<f32> = distances::get_distances(points).collect();
+            });
+        });
+    }
+    group.finish();
+}
+
+fn get_points(num_points: u64) -> Vec<Vector3<f32>> {
+    (0..num_points)
+        .map(|i| {
+            let i = i as f32;
+            Vector3 { x: i, y: i, z: i }
+        })
+        .collect()
+}
+
+criterion_group!(benches, benchmark_get_distances);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+use num_traits::Float;
+
+pub struct Vector3<T> {
+    pub x: T,
+    pub y: T,
+    pub z: T,
+}
+
+pub fn get_distances<T: Float>(points: &Vec<Vector3<T>>) -> impl Iterator<Item = T> + '_ {
+    points.iter().enumerate().flat_map(|(i, point1)| {
+        points
+            .iter()
+            .skip(i + 1)
+            .map(|point2| distance(point1, point2))
+    })
+}
+
+fn distance<T: Float>(point1: &Vector3<T>, point2: &Vector3<T>) -> T {
+    (squared_difference(point1.x, point2.x)
+        + squared_difference(point1.y, point2.y)
+        + squared_difference(point1.z, point2.z))
+    .sqrt()
+}
+
+fn squared_difference<T: Float>(a: T, b: T) -> T {
+    let difference = a - b;
+    difference * difference
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
